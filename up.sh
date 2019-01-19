d2a2de463aedd75ec08f14f720ccc498a7dd6c4c
Rizal Fakhri <rizal@kodinger.com>
2019-01-20 04:42:36 +0800

    Fix b-agent

--- up.sh
+++ up.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
-
-source ~/.bashrc
-
-# save the env into temporary file
-
-alias vpnpanel
-if [[ "$?" -ne "0" ]] ; then 
-    source ~/.bashrc
+if [[ ! -e ~/v-agent/.env ]] ; then
+    exit 1
 fi
 
-vpnpanel
-if [[ "$?" -ne "0" ]] ; then 
-    source ~/.bashrc
-fi
+SHOULDEXPORT=$(<~/v-agent/.env)
 
-
-# save the env into temp file
-TEMP_ENV=`uuidgen`
-
-echo "vpnpanel app:env:get-env" > /tmp/$TEMP_ENV
-
-source /tmp/$TEMP_ENV
-
-## DO THE AUTHENTICATION ##
-# PANEL_URL = $PANEL_URL
-# PANEL_TOKEN = $PANEL_TOKENhh
+export $SHOULDEXPORT
 
 IP_ADDRESS=`curl ipv4.icanhazip.com`
 UP_URL="$PANEL_URL/api/account/up?ip=$IP_ADDRESS&username=$username"
